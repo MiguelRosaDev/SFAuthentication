168b61e1f60662b78093157edb6f90e13bf5c495
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2023-10-03 10:20:46 +0100

    Update SFAuthSession.m

--- src/ios/SFAuthSession.m
+++ src/ios/SFAuthSession.m
@@ -38,4 +38,30 @@ SFAuthenticationSession *_authenticationVC;
     }
 }
 
+- (void)startwithoutpopup:(CDVInvokedUrlCommand *)command {
+
+    if (@available(iOS 11.0, *)) {
+        NSString* redirectScheme = [command.arguments objectAtIndex:0];
+        NSURL* requestURL = [NSURL URLWithString:[command.arguments objectAtIndex:1]];
+        SFAuthenticationSession* authenticationVC =
+        [[SFAuthenticationSession alloc] initWithURL:requestURL
+                                   callbackURLScheme:redirectScheme
+                                   completionHandler:^(NSURL * _Nullable callbackURL,
+                                                       NSError * _Nullable error) {
+                                       _authenticationVC = nil;
+                                       CDVPluginResult *result;
+                                       if (callbackURL) {
+                                           result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: callbackURL.absoluteString];
+
+                                       } else {
+                                           result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"error"];
+                                       }
+                                       [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+                                   }];
+        _authenticationVC = authenticationVC;
+        _authenticationVC.prefersEphemeralWebBrowserSession = YES;
+        [authenticationVC start];
+    }
+}
+
 @end
